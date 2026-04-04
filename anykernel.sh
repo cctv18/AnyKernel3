5e609d717c72b635866e7dda26d139576a8e334c
cctv18 <85936817+cctv18@users.noreply.github.com>
2026-04-05 04:38:40 +0800

    Update kernel string in anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=KernelSU by KernelSU Developers | Build by cctv18
+kernel.string=AnyKernel3 by KernelSU Developers | Build by cctv18
 do.devicecheck=0
 do.modules=0
 do.systemless=0
